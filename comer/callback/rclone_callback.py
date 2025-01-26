6e7ddaafe139ee8c3bd66ff526fcda7f28eb19cd
purin1410_desktop <Purin1410>
2025-01-26 10:09:01 +0700

    voiaeoi

--- comer/callback/rclone_callback.py
+++ comer/callback/rclone_callback.py
@@ -8,7 +8,7 @@ class RcloneUploadCallback(Callback):
             self.remote_dir = remote_dir  # OneDrive remote directory
 
         def on_epoch_end(self, trainer, pl_module):
-            if trainer.current_epoch %5 ==0:
+            if trainer.current_epoch %2 ==0:
                 print(f"Epoch {trainer.current_epoch} finished. Uploading checkpoints to OneDrive...")
                 # Upload all files from the local directory to OneDrive
                 command = f"rclone copy --verbose {self.local_dir} {self.remote_dir}"
@@ -16,7 +16,7 @@ class RcloneUploadCallback(Callback):
                 print(f"Upload completed for epoch {trainer.current_epoch}.")
 
         def on_train_end(self, trainer, pl_module):
-            if trainer.current_epoch %5 ==0:
+            if trainer.current_epoch %2 ==0:
                 print("Training complete. Final upload to OneDrive...")
                 # Upload one last time when training finishes
                 command = f"rclone copy --verbose {self.local_dir} {self.remote_dir}"

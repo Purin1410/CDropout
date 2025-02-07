b3c0e9eaa3424c1acdd090f17e3fe4d31a0f1245
purin1410_desktop <Purin1410>
2025-02-07 11:13:58 +0700

    vpsepe

--- comer/callback/skip_validation.py
+++ comer/callback/skip_validation.py
@@ -8,7 +8,7 @@ class SkipValidation(Callback):
     def on_validation_start(self, trainer, pl_module):
         if trainer.current_epoch >= 299:
             trainer.check_val_every_n_epoch = 10
-        elif trainer.current_epoch >= 330:
+        elif trainer.current_epoch >= 329:
             trainer.check_val_every_n_epoch = 1
         else:
             trainer.check_val_every_n_epoch = 50
@@ -16,7 +16,7 @@ class SkipValidation(Callback):
     def on_train_start(self, trainer, pl_module):
         if trainer.current_epoch >= 299:
             trainer.check_val_every_n_epoch = 10
-        elif trainer.current_epoch >= 330:
+        elif trainer.current_epoch >= 329:
             trainer.check_val_every_n_epoch = 1
         else:
             trainer.check_val_every_n_epoch = 50
